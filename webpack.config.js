b70836e8ec91776f656ce8972e4961dcc1e0829a
ljonashm <shamordinanton@gmail.com>
2021-03-01 19:05:46 +0300

    writed header, tarifs selectors, tarif card

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test:/\.(s*)css$/,
+                use: [
+                    miniCss.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    plugins: [
+        new miniCss({
+            filename: 'style.css',
+        }),
+    ]
+};
