77199892224dd90c97eecf3870702b16c7db9d45
ljonashm <shamordinanton@gmail.com>
2021-03-04 14:18:03 +0300

    finished basic layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 'use strict';
 import './styles/scss.scss';
+
+function tarifList() {
+    
+    const tarifSelectors = document.querySelectorAll('.tarif__selectors--member');
+
+    tarifSelectors.forEach(selector => {
+        selector.addEventListener('click', () => {
+            tarifSelectors.forEach(elem => {
+                if (elem.classList.contains('checked-member')) {
+                    elem.classList.remove('checked-member');
+                }
+            });
+            selector.classList.add('checked-member');
+        });
+    });
+}
+
+tarifList();
