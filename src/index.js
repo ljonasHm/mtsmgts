4ec3b3549d57411685a9a6f25aa69bd00ae68a86
ljonashm <shamordinanton@gmail.com>
2021-03-04 16:09:18 +0300

    configured moving through tariff tabs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './styles/scss.scss';
 function tarifList() {
     
     const tarifSelectors = document.querySelectorAll('.tarif__selectors--member');
+    const tarifCards = document.querySelectorAll('.tarif__list--card');
 
     tarifSelectors.forEach(selector => {
         selector.addEventListener('click', () => {
@@ -13,6 +14,14 @@ function tarifList() {
                 }
             });
             selector.classList.add('checked-member');
+            tarifCards.forEach(card => {
+                if (selector.dataset.category.indexOf(card.dataset.category) != -1
+                    && card.classList.contains('hide')) {
+                        card.classList.remove('hide')
+                } else if (!card.classList.contains('hide')) {
+                    card.classList.add('hide');
+                }
+            });
         });
     });
 }
