cd29d8c777f7df17271c8cf0c5fa437b0767ca21
ljonashm <shamordinanton@gmail.com>
2021-03-10 13:00:55 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -11,14 +11,16 @@ function tarifList() {
             tarifSelectors.forEach(elem => {
                 if (elem.classList.contains('checked-member')) {
                     elem.classList.remove('checked-member');
+                    elem.firstElementChild.classList.remove('checked--after');
                 }
             });
             selector.classList.add('checked-member');
+            selector.firstElementChild.classList.add('checked--after');
             tarifCards.forEach(card => {
-                if (selector.dataset.category.indexOf(card.dataset.category) != -1
+                if (card.dataset.category.indexOf(selector.dataset.category) != -1
                     && card.classList.contains('hide')) {
                         card.classList.remove('hide')
-                } else if (!card.classList.contains('hide')) {
+                } else if (card.dataset.category.indexOf(selector.dataset.category) == -1 && !card.classList.contains('hide')) {
                     card.classList.add('hide');
                 }
             });
@@ -26,4 +28,29 @@ function tarifList() {
     });
 }
 
+function modal() {
+    const btns = document.querySelectorAll('.card__button');
+    const modal = document.querySelector('.modal');
+    const modalWindow = document.querySelector('#modal__window');
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            modal.style.height = window.getComputedStyle(document.body).height;
+            modal.classList.remove('hide');
+            modalWindow.style.top = `${e.pageY - e.clientY + 100}px`
+        });
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal){
+            close();
+        }
+    });
+
+    function close() {
+        modal.classList.add('hide');
+    }
+}
+
 tarifList();
+modal();
